f9bf3283f34905e01fdd84d569e14c64ea97ae17
bymihail32 <bymihail32@gmail.com>
2022-09-13 11:43:53 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,99 @@
-# This is a sample Python script.
+# # this is my first Python script
+# # print("Hello, World!")
+# print("Hello, World!")
+# # print("Hello, World!")
+#
+# print(1, 4.5, 3 + 5j)
+#
+# # type()
+# # id()
+#
+# print(type(1), type(4.5), type(3 + 5j))
+# print(id(1), id(4.5), id(3 + 5j))
+#
+# # int()
+# print(int(2), int(4.7))
+#
+# # float()
+# print(float(2), float(4.7))
+#
+# # complex()
+# print(complex(2), complex(4.7), complex(3 + 5j))
+#
+# # print(7 is 7, 123456 is 123456)
+#
+#
+# # in Python null = None
+# print((5 == 5) is True)
+# print((5 == 5) is False)
+# print((5 == 5) is None)
+#
+# # 123456789 342472398472934
+# earth_sun_distance = 123456789
+# b = 342472398
+#
+# print(earth_sun_distance + b)
+# print(earth_sun_distance - b)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# a = 3
+# print('a', a, id(a), type(a))
+# a = 51
+# print('a', a, id(a), type(a))
+# a = 2.5
+# print('a', a, id(a), type(a))
+# a = True
+# print('a', a, id(a), type(a))
 
+# a = 3
+# print('a', a)
+# a += 5  # a = a + 5
+# print('a', a)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# print('This is a string')
+# print("This is a string")
 
+# print("That's my dog")
+# print('My father said: "Go to work!"')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# print('a\tb')  # NOT: a\tb
+# print('a\bb')  # a\bb
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print('That\'s my dog')
+
+# print('\\\\t is a special character')
+
+# print(r'\t is a special character')
+
+# my_age = 30
+# my_string = 'My age is ' + str(my_age) + '.'
+# print('my_string', my_string)
+
+# my_name = "John"
+# my_age = 20
+# my_string = 'My age is X.'
+# my_string = 'My name is %s and I am %s years old.' % (my_name, my_age)
+# my_string = 'My name is {} and I am {} years old.'.format(my_name, my_age)
+# my_string = 'My name is {0} and I am {1} years old.'.format(my_name, my_age)
+# my_string = 'My name is {name} and I am {age} years old.'.format(age=my_age, name=my_name)
+# my_string = 'My name is {my_name} and I am {my_age} years old.'.format(my_age=my_age, my_name=my_name)
+# my_string = f'My name is {my_name} and I am {my_age} years old.'
+# print(my_string)
+
+# price = 23.50
+# print(f'Total price = {price:.2f} RON')
+
+# print('Row 1\nRow 2\nRow 3')
+# print("""Row 1
+#     Row 2
+# Row 3""")
+# print('''Row 1 "'
+# Row 2
+# Row 3''')
+
+my_string = 'orice string'
+#            01234567891011
+# print(len(my_string))
+# print(my_string[3], my_string)
+
+# print('rice' not in my_string)
+# print('ricee' not in my_string)
