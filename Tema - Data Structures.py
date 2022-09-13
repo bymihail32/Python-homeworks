75dfd5801854b503ace59b01301f081469288b25
bymihail32 <bymihail32@gmail.com>
2022-09-13 16:37:10 +0300

    Homework commit

--- Tema - Data Structures.py
+++ Tema - Data Structures.py
@@ -1,21 +1,35 @@
+# Declararea listei cu elementele continute:
+
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+
+print("My list:", my_list)
+
+# Lista ordonata ascendent
 
 my_list.sort()
 
-print(my_list)
+print("List in ascending order:", my_list)
+
+# Lista ordonata descendent
 
 my_list.sort(reverse=True)
 
-print(my_list)
+print("List in descending order:", my_list)
+
+# Lista cu numerele pare
 
 my_sliced_list = my_list[-2::-2]
 
-print(my_sliced_list)
+print("List of even numbers:", my_sliced_list)
+
+# Lista cu numerele impare
 
 my_sliced_list = my_list[::-2]
 
-print(my_sliced_list)
+print("List of odd numbers:", my_sliced_list)
+
+# Lista cu multipli lui 3
 
 my_sliced_list = my_list[-3::-3]
 
-print(my_sliced_list)
+print("List of multiples of 3:", my_sliced_list)
