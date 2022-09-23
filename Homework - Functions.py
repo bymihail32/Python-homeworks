7eb5a8f9da38adcd8476de88c386db5d107e1dfa
bymihail32 <Azazel85>
2022-09-23 17:18:18 +0300

    Another test

--- Homework - Functions.py
+++ Homework - Functions.py
@@ -1,4 +1,14 @@
 # Exercises 1:
+def number(a: 0, b, c, *args, **kwargs):
+    print(a + b + c)
+
+number(1, 5, -3, 'abc', [12, 56, 'cad'])
+
+def new_function(a=0, b=1, c=2, d=3, e=4):
+
+    return a + b + c
+print(new_function(1, 5, -3, 'abc', [12, 56, 'cad']))
+
 
 def my_function(a=1, b=5, c=-3, d='abc', e=[12, 56, 'cad']):
     return a + b - c
